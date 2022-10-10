e8cf8417f80259c80fd71f958c7f5172b5010707
JackyLook003 <jackylook003@gmail.com>
2022-10-09 22:35:25 -0400

    "first commit for basic"

---
+++ lecture note/lec1.java
@@ -0,0 +1,19 @@
+lecture 1:
+interface = list of functions/methods to be implemented.
+
+public class : open to public
+
+object: inherited by any class/ no superclas
+
+
+
+xxx = New (constructor)
+
+
+class constructor:
+publuc class_name(){
+
+
+}
+
+
